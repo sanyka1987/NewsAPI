49ac13061f03753687fdfdbf3640d5b46595ea07
sanyka1987 <123751492+sanyka1987@users.noreply.github.com>
2024-05-19 14:37:09 +0300

    Models
    News Models

---
+++ NewsApi/Models/News.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.Models
+{
+    public class News
+    {
+        [JsonProperty("source")]
+        public Source Source { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("urlToImage")]
+        public string UrlToImage { get; set; }
+
+        [JsonProperty("publishedAt")]
+        public DateTime PublishedAt { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+    }
+
+    public class NewsResponse
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("totalResults")]
+        public int TotalResults { get; set; }
+
+        [JsonProperty("articles")]
+        public List<News> Articles { get; set; }
+    }
+
+    public class Source
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}

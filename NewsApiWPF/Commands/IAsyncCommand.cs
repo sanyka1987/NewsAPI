d64f9cf21ada776687b925d4bc48ebc4e8b3cc86
sanyka1987 <123751492+sanyka1987@users.noreply.github.com>
2024-05-19 21:25:24 +0300

    New Service and Command
    Added asynchronous commands and services for data download, news search, "Read more" command service

---
+++ NewsApiWPF/Commands/IAsyncCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NewsApiWPF.Commands
+{
+    public interface IAsyncCommand<T> : ICommand
+    {
+        Task ExecuteAsync(T parameter);
+    }
+}
--- NewsApiWPF/MainWindow.xaml.cs
+++ NewsApiWPF/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using NewsAPI.Constants;
+using NewsAPI.Models;
+using NewsApiWPF.Commands;
+using NewsApiWPF.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +20,44 @@ using System.Windows.Shapes;
 
 namespace NewsApiWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly NewsService _newsService;
+        private readonly SearchService _searchService;
+        private readonly CommandService _commandService;
+        private readonly DownloadNewsService _downloadNewsService;
+
+        public ObservableCollection<Article> NewsCollection { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            _newsService = new NewsService();
+            _searchService = new SearchService(_newsService);
+            _commandService = new CommandService();
+            _downloadNewsService = new DownloadNewsService(_newsService);
+
+            DataContext = this;
+            NewsCollection = new ObservableCollection<Article>(); // Ініціалізація колекції
+
+            _searchService.InitializeSearch(NewsCollection);
+            LoadNewsAsync(); // Виклик методу завантаження даних
         }
+
+        private async Task LoadNewsAsync()
+        {
+            await _downloadNewsService.DownloadArticlesAsync(NewsCollection);
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var query = SearchTextBox.Text;
+            _searchService.OnSearchTextChanged(query);
+        }
+
+        public IAsyncCommand<Article> ReadMoreCommand => new AsyncCommand<Article>(async (article) =>
+        {
+            await _commandService.OpenUrlAsync(article?.Url);
+        });
     }
 }
